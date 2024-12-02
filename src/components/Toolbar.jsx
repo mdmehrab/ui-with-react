380a71d447b7a0b481d26ec452288235f27b3136
mehrab-rakib <ishraqmehrab2002@gmail.com>
2024-12-02 10:12:59 +0600

    complete the toolbar

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,43 +1,43 @@
-import { GrLanguage } from "react-icons/gr";
-import { MdOutlineShoppingCart } from "react-icons/md";
+import { GrLanguage } from 'react-icons/gr';
+import { MdOutlineShoppingCart } from 'react-icons/md';
 // import { FaSearch } from "react-icons/fa";
 
 const Toolbar = () => {
   return (
-    <div className="py-3">
-      <div className="grid grid-cols-7">
-        <div className="col-span-4 flex gap-4 justify-center items-center">
-          <p className="font-bold text-xl">Udemy</p>
-          <p className="text-gray-400">category</p>
-          <div>
-            {/* <span>
+    <div className="grid grid-cols-9 py-3">
+      <div className="col-span-6 flex gap-7 justify-center items-center">
+        <p className="font-bold text-2xl">Udemy</p>
+        <p className="text-gray-400">category</p>
+        <div>
+          {/* <span>
               <FaSearch />
             </span> */}
-            <input
-              type="search"
-              placeholder="search for anything"
-              className="border border-black p-3 w-[600px] rounded-[100px] focus:outline-none"
-            />
-          </div>
+          <input
+            type="search"
+            placeholder="search for anything"
+            className="border border-black p-3 w-[830px] rounded-[100px] focus:outline-none"
+          />
         </div>
+      </div>
 
-        <div className="col-span-3 flex justify-center items-center gap-7">
-          <p>udemy bunesiness</p>
-          <p>teach on udemy</p>
-          <p>
+      <div className="col-span-3 flex justify-center items-center gap-x-3 ">
+        <p>udemy bunesiness</p>
+        <p>teach on udemy</p>
+        <p>
+          <span className="text-2xl">
             <MdOutlineShoppingCart />
-          </p>
-          <div className="gap-2 flex">
-            <button className="border border-bllack py-1 px-3">log in</button>
-            <button className="bg-black text-white py-1 px-3">sign in</button>
-          </div>
-
-          <p className="border border-black p-2">
-            <span className="">
-              <GrLanguage />
-            </span>
-          </p>
+          </span>
+        </p>
+        <div className="gap-2 flex">
+          <button className="border border-bllack py-1 px-3">log in</button>
+          <button className="bg-black text-white py-1 px-3">sign in</button>
         </div>
+
+        <p className="border border-black p-2">
+          <span className="">
+            <GrLanguage />
+          </span>
+        </p>
       </div>
     </div>
   );
